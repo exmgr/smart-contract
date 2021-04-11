2e853a825c74d5b60ecda1a5387adceb27afce14
Manolis Nikiforakis <manolis@weatherxm.com>
2021-04-11 17:41:31 +0300

    cleaning

--- EXMWeatherConsumer.sol
+++ EXMWeatherConsumer.sol
@@ -11,19 +11,7 @@ contract EXMWeatherConsumer is ChainlinkClient {
     uint256 public result;
     
     string private weatherGeohash;
-    
-    /**
-     * Network: Kovan
-     * Oracle: 
-     *      Name:           Alpha Chain - Kovan
-     *      Listing URL:    https://market.link/nodes/ef076e87-49f4-486b-9878-c4806781c7a0?start=1614168653&end=1614773453
-     *      Address:        0xAA1DC356dc4B18f30C347798FD5379F3D77ABC5b
-     * Job: 
-     *      Name:           OpenWeather Data
-     *      Listing URL:    https://market.link/jobs/e10388e6-1a8a-4ff5-bad6-dd930049a65f
-     *      ID:             235f8b1eeb364efc83c26d0bef2d0c01
-     *      Fee:            0.1 LINK
-     */
+
     constructor() public {
         setPublicChainlinkToken();
         oracle = 0xe4C58E236C6967922d2F0Dd2d80DCb3F6b1b2f49;
